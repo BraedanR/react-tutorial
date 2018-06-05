48bbc664ab571459168b782dd53f123682a31716
Braedan Robinson <Brae@Braedans-MacBook-Pro.local>
2018-06-05 11:14:27 -0400

    08-state

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,31 +1,37 @@
 import React from "react";
 
 export class Home extends React.Component {
+
+    constructor(props) {
+        super();
+        this.state = {
+            age: props.initialAge,
+            status: 0
+        };
+    }
+
+    onMakeOlder() {
+        this.setState({
+            age: this.state.age + 3
+        });
+    }
+
     render() {
-        var text = "Something!";
 
         return(
             <div>
                 <p>New component</p>
-                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-                <p> User Object => Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    <ul>
-                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
-                <p>{text}</p>
+                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
                 <hr/>
-                {this.props.children}
+                <button onClick={() => this.onMakeOlder()} className="btn btn-primary"> Make me older!</button>
             </div>
         );
     }
 }
+
 // Can't read property of undefined
-// Home.PropTypes = {
+// Home.propTypes = {
 //     name: React.PropTypes.string,
-//     age: React.PropTypes.number,
-//     user: React.PropTypes.object,
-//     children: React.PropTypes.element.isRequired
+//     initialAge: React.PropTypes.number
 // };
