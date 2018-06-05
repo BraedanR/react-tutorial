05b923fef503cb4a1d664e6879f415fbd55a5989
Braedan Robinson <Brae@Braedans-MacBook-Pro.local>
2018-06-05 10:54:15 -0400

    06 push`

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,17 +2,30 @@ import React from "react";
 
 export class Home extends React.Component {
     render() {
-        let content = ""
-        if (true) {
-            content = <p>Hello from if!</p>;
-        }
+        var text = "Something!";
+
         return(
             <div>
                 <p>New component</p>
-                {content}
-                {"Hello from string"}
-                {5 == 2 ? "Yes" : "No"}
+                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
+                <p> User Object => Name: {this.props.user.name}</p>
+                <div>
+                    <h4>Hobbies</h4>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                    </ul>
+                </div>
+                <p>{text}</p>
+                <hr/>
+                {this.props.children}
             </div>
         );
     }
 }
+// Can't read property of undefined
+// Home.PropTypes = {
+//     name: React.PropTypes.string,
+//     age: React.PropTypes.number,
+//     user: React.PropTypes.object,
+//     children: React.PropTypes.element.isRequired
+// };
--- src/app/index.js
+++ src/app/index.js
@@ -6,16 +6,22 @@ import { Home } from "./components/Home";
 
 class App extends React.Component {
   render () {
-    return (
-      <div className="container">
-        <div className="row">
-            <div className="col-xs-10 col-xs-offset-1">
-                <Header/>
+      var user = {
+          name: "Anna",
+          hobbies: ['Sports', 'Reading']
+      };
+      return (
+          <div className="container">
+            <div className="row">
+                <div className="col-xs-10 col-xs-offset-1">
+                    <Header/>
+                </div>
             </div>
-        </div>
         <div className="row">
             <div className="col-xs-10 col-xs-offset-1">
-                <Home/>
+                <Home name={"Max"} age={27} user={user}>
+                    <p>This is a paragraph</p>
+                </Home>
             </div>
         </div>
       </div>
